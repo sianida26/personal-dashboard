d3be182f82caf82b3c6d5d5bc7192194e279e167
sianida26 <chesakeselekkebo@gmail.com>
2024-05-05 13:38:09 +0700

    Replace hard coded auth token

--- apps/frontend/src/honoClient.ts
+++ apps/frontend/src/honoClient.ts
@@ -3,7 +3,7 @@ import { AppType } from "backend";
 
 const client = hc<AppType>("http://localhost:3000", {
 	headers: {
-		Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJkeG5icHQ0N3BwdXZ6NW1vbTF1NjF5cnMiLCJpYXQiOjE3MTQ1NjY4MDB9.920-iiSuLiPK0t0GiWIuT_n8BHngPxp1FyvYZ7SBJ7E`,
+		Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
 	},
 });
 
