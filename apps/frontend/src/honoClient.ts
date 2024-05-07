721fd67c48d3fe5d09d46f76a17dd9289c2d30e4
sianida26 <chesakeselekkebo@gmail.com>
2024-05-08 00:46:16 +0700

    Added env check

--- apps/frontend/src/honoClient.ts
+++ apps/frontend/src/honoClient.ts
@@ -1,7 +1,11 @@
 import { hc } from "hono/client";
 import { AppType } from "backend";
 
-const client = hc<AppType>("http://localhost:3000", {
+const backendUrl = import.meta.env.VITE_BACKEND_BASE_URL as string | undefined;
+
+if (!backendUrl) throw new Error("Backend URL not set");
+
+const client = hc<AppType>(backendUrl, {
 	headers: {
 		Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
 	},
