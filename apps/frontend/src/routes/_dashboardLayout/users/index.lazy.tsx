e5800bd82d518cf4e8a652821a29437145ad5c49
sianida26 <chesakeselekkebo@gmail.com>
2024-06-11 03:52:10 +0700

    Added user modal table contents

--- apps/frontend/src/routes/_dashboardLayout/users/index.lazy.tsx
+++ apps/frontend/src/routes/_dashboardLayout/users/index.lazy.tsx
@@ -2,8 +2,12 @@ import { userQueryOptions } from "@/modules/usersManagement/queries/userQueries"
 import PageTemplate from "@/components/PageTemplate";
 import { createLazyFileRoute } from "@tanstack/react-router";
 import UserFormModal from "@/modules/usersManagement/modals/UserFormModal";
-import { createColumnHelper } from "@tanstack/react-table";
 import ExtractQueryDataType from "@/types/ExtractQueryDataType";
+import { createColumnHelper } from "@tanstack/react-table";
+import { Badge, Flex } from "@mantine/core";
+import createActionButtons from "@/utils/createActionButton";
+import { TbEye, TbPencil, TbTrash } from "react-icons/tb";
+import UserDeleteModal from "@/modules/usersManagement/modals/UserDeleteModal";
 
 export const Route = createLazyFileRoute("/_dashboardLayout/users/")({
 	component: UsersPage,
@@ -18,21 +22,62 @@ export default function UsersPage() {
 		<PageTemplate
 			title="Users"
 			queryOptions={userQueryOptions}
-			modals={[<UserFormModal />]}
+			modals={[<UserFormModal />, <UserDeleteModal />]}
 			columnDefs={[
 				columnHelper.display({
-					id: "sequence",
 					header: "#",
 					cell: (props) => props.row.index + 1,
-					size: 1,
 				}),
 
-				columnHelper.accessor("email", {
-					header: "Email",
+				columnHelper.display({
+					header: "Name",
+					cell: (props) => props.row.original.name,
 				}),
 
-				columnHelper.accessor("email", {
-					header: "Email",
+				columnHelper.display({
+					header: "Username",
+					cell: (props) => props.row.original.username,
+				}),
+
+				columnHelper.display({
+					header: "Status",
+					cell: (props) =>
+						props.row.original.isEnabled ? (
+							<Badge color="green">Active</Badge>
+						) : (
+							<Badge color="red">Inactive</Badge>
+						),
+				}),
+
+				columnHelper.display({
+					header: "Actions",
+					cell: (props) => (
+						<Flex gap="xs">
+							{createActionButtons([
+								{
+									label: "Detail",
+									permission: true,
+									action: `?detail=${props.row.original.id}`,
+									color: "green",
+									icon: <TbEye />,
+								},
+								{
+									label: "Edit",
+									permission: true,
+									action: `?edit=${props.row.original.id}`,
+									color: "orange",
+									icon: <TbPencil />,
+								},
+								{
+									label: "Delete",
+									permission: true,
+									action: `?delete=${props.row.original.id}`,
+									color: "red",
+									icon: <TbTrash />,
+								},
+							])}
+						</Flex>
+					),
 				}),
 			]}
 		/>
