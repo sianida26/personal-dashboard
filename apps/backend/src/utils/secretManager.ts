884c07bf56c7c2174a620c5216f561ee998acea2
sianida26 <chesakeselekkebo@gmail.com>
2024-08-24 22:30:20 +0700

    Created JSDoc

--- apps/backend/src/utils/secretManager.ts
+++ apps/backend/src/utils/secretManager.ts
@@ -3,6 +3,11 @@ import appLogger from "./logger";
 import * as fs from "fs";
 import * as crypto from "crypto";
 
+/**
+ * Generates an RSA key pair using the specified options.
+ *
+ * @returns An object containing the generated `privateKey` and `publicKey` in PEM format.
+ */
 export const generateKeyPair = () => {
 	const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
 		modulusLength: 2048,
@@ -19,6 +24,12 @@ export const generateKeyPair = () => {
 	return { privateKey, publicKey };
 };
 
+/**
+ * Creates RSA key pair files for the private and public keys.
+ * If the key pair is successfully generated, it writes them to the specified file paths.
+ *
+ * @returns An object containing the `privateKey` and `publicKey`.
+ */
 const createKeyPairFiles = () => {
 	appLogger.info("Creating public/private key pair...");
 
@@ -34,6 +45,12 @@ const createKeyPairFiles = () => {
 	return { privateKey, publicKey };
 };
 
+/**
+ * Retrieves the private key from the specified file path.
+ * If the private key does not exist, it generates a new key pair and returns the private key.
+ *
+ * @returns The `privateKey` as a string in PEM format.
+ */
 export const getPrivateKey = () => {
 	if (fs.existsSync(appEnv.PRIVATE_KEY_PATH)) {
 		return fs.readFileSync(appEnv.PRIVATE_KEY_PATH, "utf-8");
@@ -44,6 +61,12 @@ export const getPrivateKey = () => {
 	}
 };
 
+/**
+ * Retrieves the public key from the specified file path.
+ * If the public key does not exist, it generates a new key pair and returns the public key.
+ *
+ * @returns The `publicKey` as a string in PEM format.
+ */
 export const getPublicKey = () => {
 	if (fs.existsSync(appEnv.PUBLIC_KEY_PATH)) {
 		return fs.readFileSync(appEnv.PUBLIC_KEY_PATH, "utf-8");
