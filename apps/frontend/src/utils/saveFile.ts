24d84a30d604cb6562f4bac222d539a6511f99cf
sianida26 <chesakeselekkebo@gmail.com>
2024-08-31 00:45:32 +0700

    feat: Added save file util function

---
+++ apps/frontend/src/utils/saveFile.ts
@@ -0,0 +1,22 @@
+import { writeFile } from "fs/promises";
+
+/**
+ * Saves a file to the specified path on the filesystem.
+ *
+ * This function accepts either a `File` object (commonly used in the browser) or a `Buffer`
+ * and writes the content to the specified path.
+ *
+ * @param path - The file system path where the file should be saved.
+ * @param file - The file data to be saved. It can be either a `File` object or a `Buffer`.
+ * @returns A promise that resolves when the file has been successfully written.
+ *
+ * @throws If the file writing fails, the promise will be rejected with an error.
+ */
+const saveFile = async (path: string, file: File | Buffer) => {
+	const buffer =
+		file instanceof File ? Buffer.from(await file.arrayBuffer()) : file;
+
+	await writeFile(path, buffer);
+};
+
+export default saveFile;
