c6b566abca655f16e4fe265b343eeebaf52aaddb
sianida26 <chesakeselekkebo@gmail.com>
2024-12-21 14:15:25 +0700

    feat: add normalizePhoneNumber utility function for standardizing phone number formats

---
+++ apps/backend/src/utils/normalizePhoneNumber.ts
@@ -0,0 +1,20 @@
+function normalizePhoneNumber(
+	phone: string,
+	defaultCountryCode: string = "62"
+): string {
+	// Remove all spaces and non-digit characters except "+"
+	phone = phone.replace(/[^+\d]/g, "");
+
+	if (phone.startsWith("+")) {
+		// If it starts with a "+", remove the "+" to standardize
+		phone = phone.substring(1);
+	} else if (phone.startsWith("0")) {
+		// If it starts with "0", prepend the default country code
+		phone = defaultCountryCode + phone.substring(1);
+	} else if (!phone.startsWith(defaultCountryCode)) {
+		// If it doesn't start with the default country code or another country code
+		phone = defaultCountryCode + phone;
+	}
+
+	return phone;
+}
