39974d720261256c9b31f88907d6d879efc5bbff
sianida26 <chesakeselekkebo@gmail.com>
2025-01-26 23:20:56 +0700

    feat: add schema generator with soft delete option and update formatting settings

--- apps/backend/turbo/generators/config.ts
+++ apps/backend/turbo/generators/config.ts
@@ -3,6 +3,47 @@ import type { PlopTypes } from "@turbo/gen";
 // Learn more about Turborepo Generators at https://turbo.build/repo/docs/core-concepts/monorepos/code-generation
 
 export default function generator(plop: PlopTypes.NodePlopAPI) {
+
+	plop.setGenerator("add-schema", {
+		description: "Adds a new schema to the DB",
+		prompts: [
+			{
+				type: "input",
+				name: "name",
+				message: "What is the name of the schema? (use plural name)",
+			},
+			{
+				type: "confirm",
+				name: "hasSoftDelete",
+				message: "Does this schema have soft delete?",
+			},
+		],
+		actions: (data) => [
+			// Add the schema file
+			{
+				type: "add",
+				path: "src/drizzle/schema/{{camelCase name}}.ts",
+				templateFile: "templates/db-schema.hbs",
+			},
+
+			// Import the schema in index.ts
+			{
+				type: "append",
+				path: "src/drizzle/index.ts",
+				pattern: /import \* as usersSchema from ".\/schema\/users";/g,
+				template: `import * as {{camelCase name}}Schema from "./schema/{{camelCase name}}";`,
+			},
+
+			// Add the schema to the db object
+			{
+				type: "append",
+				path: "src/drizzle/index.ts",
+				pattern: /schema: {/g,
+				template: "\t\t...{{camelCase name}}Schema,",
+			},
+		],
+	});
+
 	plop.setGenerator("basic-crud", {
 		description: "Adds a Basic CRUD route API",
 		prompts: [
@@ -44,7 +85,8 @@ export default function generator(plop: PlopTypes.NodePlopAPI) {
 				type: "append",
 				path: "src/index.ts",
 				pattern: /\.route\("\/dev", devRoutes\)/g,
-				template: '\t.route("/{{kebabCase name}}", {{camelCase name}}Route)',
+				template:
+					'\t.route("/{{kebabCase name}}", {{camelCase name}}Route)',
 			},
 		],
 	});
