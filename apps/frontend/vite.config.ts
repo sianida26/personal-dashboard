e5f0d65678d555f09fe92a8b4e9be3d24290a888
sianida26 <chesakeselekkebo@gmail.com>
2025-02-16 14:13:48 +0700

    feat: add Tailwind CSS Vite plugin and update styles

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,12 +1,13 @@
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig, loadEnv } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 process.env = { ...process.env, ...loadEnv("", process.cwd()) };
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), TanStackRouterVite()],
+	plugins: [react(), TanStackRouterVite(), tailwindcss()],
 	resolve: {
 		alias: {
 			"@": "/src",
