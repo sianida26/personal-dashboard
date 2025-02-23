70222f762b0e5316d2172e272c84d88ba4128e07
sianida26 <chesakeselekkebo@gmail.com>
2025-02-23 12:49:09 +0700

    feat: add showEmpty prop to Label component to control rendering of empty children

--- packages/ui/src/components/label.tsx
+++ packages/ui/src/components/label.tsx
@@ -5,27 +5,34 @@ import * as React from "react";
 import { cn } from "../utils";
 
 const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+	"text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
 );
 
 type LabelProps = React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
-  VariantProps<typeof labelVariants> & {
-    withAsterisk?: boolean;
-  };
+	VariantProps<typeof labelVariants> & {
+		withAsterisk?: boolean;
+		showEmpty?: boolean; // when true, render even if children is empty
+	};
 
 const Label = React.forwardRef<
-  React.ComponentRef<typeof LabelPrimitive.Root>,
-  LabelProps
->(({ className, withAsterisk, children, ...props }, ref) => (
-  <LabelPrimitive.Root
-    ref={ref}
-    className={cn(labelVariants(), className)}
-    {...props}
-  >
-    {children}
-    {withAsterisk && <span className="text-red-500">*</span>}
-  </LabelPrimitive.Root>
-));
+	React.ComponentRef<typeof LabelPrimitive.Root>,
+	LabelProps
+>(({ className, withAsterisk, children, showEmpty, ...props }, ref) => {
+	if (!children && !showEmpty) {
+		return null;
+	}
+
+	return (
+		<LabelPrimitive.Root
+			ref={ref}
+			className={cn(labelVariants(), className)}
+			{...props}
+		>
+			{children}
+			{withAsterisk && <span className="text-red-500">*</span>}
+		</LabelPrimitive.Root>
+	);
+});
 Label.displayName = LabelPrimitive.Root.displayName;
 
 export { Label };
