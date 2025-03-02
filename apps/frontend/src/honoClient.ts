723ca564a7608d42fd190f2dca287a9720ad508a
sianida26 <chesakeselekkebo@gmail.com>
2025-03-03 06:14:38 +0700

    fix: update authorization header to use access token from indexedDB

--- apps/frontend/src/honoClient.ts
+++ apps/frontend/src/honoClient.ts
@@ -1,16 +1,18 @@
 import type { AppType } from "backend";
 import { hc } from "hono/client";
+import { authDB } from "./indexedDB/authDB";
 
 const backendUrl = import.meta.env.VITE_BACKEND_BASE_URL as string | undefined;
-
-console.log(backendUrl);
 
 if (!backendUrl) throw new Error("Backend URL not set");
 
 const client = hc<AppType>(backendUrl, {
-	headers: () => ({
-		Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-	}),
+	headers: async () => {
+		const authData = await authDB.auth.get("auth");
+		return {
+			Authorization: `Bearer ${authData?.accessToken ?? ""}`,
+		};
+	},
 });
 
 export default client;
