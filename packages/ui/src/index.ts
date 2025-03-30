5c13b0c78858f40e93abb94879259c3dd52081b2
sianida26 <chesakeselekkebo@gmail.com>
2025-03-30 11:43:39 +0700

    feat: add avatar component export to UI index

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./components/accordion";
 export * from "./components/alert-dialog";
 export * from "./components/alert";
+export * from "./components/avatar";
 export * from "./components/badge";
 export * from "./components/button";
 export * from "./components/calendar";
